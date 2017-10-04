33f45e7b55d7212baa13e1a64ee4194e3515dd55
facundo mila <facundo.mila@globant.com>
2017-10-04 18:49:43 -0300

    refactor navigation bar

--- components/core-components/navigation-bar.js
+++ components/core-components/navigation-bar.js
@@ -1,15 +1,14 @@
 var React = require('react');
 var Icon = require('components/core-components/icon');
 var Link = require('components/core-components/link');
+var NavigationSticky = require('components/core-components/navigation-sticky');
 
 var NavigationBar = React.createClass({
 
     getInitialState: function() {
         return {
           mobileNav: 'navigation-bar',
-          burguerButton: 'open',
-          stickyBarClass: 'navigation-sticky--hidden',
-          stickyBarState: false
+          burguerButton: 'open'
         }
     },
 
@@ -21,108 +20,97 @@ var NavigationBar = React.createClass({
       // </div>
         return (
             <div className="navigation">
-              {this.handleScroll()}
-              {this.renderStickyBar()}
-              <div className="navigation-logo-container"></div>
-              <button {...this.getBurgerButtonProps()}>
-                {this.getIcon()}
-              </button>
-              <a href="demo.html">
-                <img className="navigation-logo" src="images/iso_blanco.png" />
-              </a>
-                <nav>
-                    {this.getLinks()}
-                </nav>
+              <NavigationSticky />
             </div>
         );
-    },
-
-    renderStickyBar: function () {
-        var sticky = <div className={this.state.stickyBarClass}>
-                      <div className="navigation-logo-container--sticky">
-                        <a href="demo.html">
-                          <img className="navigation-logo--sticky" src="images/iso_blanco-small.png" />
-                        </a>
-                      </div>
-                      {this.getLinks()}
-                      <div className="navigation-networks--sticky">
-                        <Link kind="no_bubble" linkType="facebook"/>
-                        <Link kind="no_bubble" linkType="twitter"/>
-                        <Link kind="no_bubble" linkType="instagram"/>
-                      </div>
-                    </div>;
-
-        var renderStiky = (this.state.stickyBarState) ? sticky : null;
-
-        return renderStiky;
-    },
-
-    handleScroll: function () {
-        window.addEventListener('scroll', this.getStickyBar)
-    },
-
-    getStickyBar: function (e) {
-        var scrollY = e.path[0].scrollY || e.path[1].scrollY;
-        console.log(scrollY)
-        if (scrollY >= 50) {
-          this.setState({ stickyBarState: true })
-          this.setState({ stickyBarClass: 'navigation-sticky--hidden'})
-        } if (scrollY >= 120) {
-          this.setState({ stickyBarClass: 'navigation-sticky--shown'})
-        } if (scrollY <= 120) {
-          this.setState({ stickyBarClass: 'navigation-sticky--hidden'})
-        } if (scrollY <= 120) {
-          this.setState({ stickyBarState: false})
-        }
-
-        return scrollY;
-    },
-
-    getIcon: function () {
-      return (this.state.burguerButton === 'open') ? <Icon type="bars" /> : <Icon type="close" />;
-    },
-
-    getBurgerButtonProps: function () {
-      var props = {
-        className: 'navigation-bar-burger-btn',
-        onClick: this.handleClick
-      }
-      return props;
-    },
-
-    getLinks: function () {
-        return (
-          <div className={this.state.mobileNav}>
-            <div className="navigation-bar-links"><a href="#">Novedades</a></div>
-            <div className="navigation-bar-links"><a href="#">Deportes</a></div>
-            <div className="navigation-bar-links"><a href="#">Life Style</a></div>
-            <div className="navigation-bar-links"><a href="#">Turismo</a></div>
-            {this.renderLogoBar()}
-            <div className="navigation-bar-links"><a href="#">Ocio</a></div>
-            <div className="navigation-bar-links"><a href="#">Tecno</a></div>
-            <div className="navigation-bar-links"><a href="#">Sociales</a></div>
-            <div className="navigation-bar-links"><a href="#">Concepto Tv</a></div>
-          </div>
-        );
-    },
-
-    handleClick: function() {
-      (this.state.mobileNav === 'navigation-bar') ?
-      this.setState({ mobileNav: 'navigation-bar--opened' }) :
-      this.setState({ mobileNav: 'navigation-bar'});
-
-      (this.state.mobileNav === 'navigation-bar') ?
-      this.setState({ burguerButton: 'close' }) :
-      this.setState({ burguerButton: 'open'});
-    },
-
-    renderLogoBar: function () {
-      console.log(this.state.mobileNav);
-      var img = <div className={this.state.stickyBarClass+"--logo"}><img className={this.state.stickyBarClass+"--img"} src="images/logo_blanco_bar.png"/></div>;
-      var renderlogo = (this.state.mobileNav === "navigation-bar") ? img : null;
-
-      return renderlogo;
     }
+
+    // renderStickyBar: function () {
+    //     var sticky = <div className={this.state.stickyBarClass}>
+    //                   <div className="navigation-logo-container--sticky">
+    //                     <a href="demo.html">
+    //                       <img className="navigation-logo--sticky" src="images/iso_blanco-small.png" />
+    //                     </a>
+    //                   </div>
+    //                   {this.getLinks()}
+    //                   <div className="navigation-networks--sticky">
+    //                     <Link kind="no_bubble" linkType="facebook"/>
+    //                     <Link kind="no_bubble" linkType="twitter"/>
+    //                     <Link kind="no_bubble" linkType="instagram"/>
+    //                   </div>
+    //                 </div>;
+    //
+    //     var renderStiky = (this.state.stickyBarState) ? sticky : null;
+    //
+    //     return renderStiky;
+    // },
+    //
+    // handleScroll: function () {
+    //     window.addEventListener('scroll', this.getStickyBar)
+    // },
+    //
+    // getStickyBar: function (e) {
+    //     var scrollY = e.path[0].scrollY || e.path[1].scrollY;
+    //     console.log(scrollY)
+    //     if (scrollY >= 50) {
+    //       this.setState({ stickyBarState: true })
+    //       this.setState({ stickyBarClass: 'navigation-sticky--hidden'})
+    //     } if (scrollY >= 120) {
+    //       this.setState({ stickyBarClass: 'navigation-sticky--shown'})
+    //     } if (scrollY <= 120) {
+    //       this.setState({ stickyBarClass: 'navigation-sticky--hidden'})
+    //     } if (scrollY <= 120) {
+    //       this.setState({ stickyBarState: false})
+    //     }
+    //
+    //     return scrollY;
+    // },
+    //
+    // getIcon: function () {
+    //   return (this.state.burguerButton === 'open') ? <Icon type="bars" /> : <Icon type="close" />;
+    // },
+    //
+    // getBurgerButtonProps: function () {
+    //   var props = {
+    //     className: 'navigation-bar-burger-btn',
+    //     onClick: this.handleClick
+    //   }
+    //   return props;
+    // },
+    //
+    // getLinks: function () {
+    //     return (
+    //       <div className={this.state.mobileNav}>
+    //         <div className="navigation-bar-links"><a href="#">Novedades</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Deportes</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Life Style</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Turismo</a></div>
+    //         {this.renderLogoBar()}
+    //         <div className="navigation-bar-links"><a href="#">Ocio</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Tecno</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Sociales</a></div>
+    //         <div className="navigation-bar-links"><a href="#">Concepto Tv</a></div>
+    //       </div>
+    //     );
+    // },
+    //
+    // handleClick: function() {
+    //   (this.state.mobileNav === 'navigation-bar') ?
+    //   this.setState({ mobileNav: 'navigation-bar--opened' }) :
+    //   this.setState({ mobileNav: 'navigation-bar'});
+    //
+    //   (this.state.mobileNav === 'navigation-bar') ?
+    //   this.setState({ burguerButton: 'close' }) :
+    //   this.setState({ burguerButton: 'open'});
+    // },
+    //
+    // renderLogoBar: function () {
+    //   console.log(this.state.mobileNav);
+    //   var img = <div className={this.state.stickyBarClass+"--logo"}><img className={this.state.stickyBarClass+"--img"} src="images/logo_blanco_bar.png"/></div>;
+    //   var renderlogo = (this.state.mobileNav === "navigation-bar") ? img : null;
+    //
+    //   return renderlogo;
+    // }
 });
 
 module.exports = NavigationBar;
---
+++ components/core-components/navigation-sticky.js
@@ -0,0 +1,63 @@
+var React = require('react');
+var Link = require('components/core-components/link');
+
+var NavigationSticky = React.createClass({
+
+    getInitialState: function() {
+        return {
+          stickyBarClass: 'navigation-sticky--hidden',
+        }
+    },
+
+    render: function () {
+      return (
+          <div className={this.state.stickyBarClass}>
+              {this.handleScroll()}
+              <div className="navigation-logo-container--sticky">
+                  <a href="demo.html">
+                      <img className="navigation-logo--sticky" src="images/iso_blanco-small.png" />
+                  </a>
+              </div>
+                  {this.getLinks()}
+              <div className="navigation-networks--sticky">
+                  <Link kind="no_bubble" linkType="facebook"/>
+                  <Link kind="no_bubble" linkType="twitter"/>
+                  <Link kind="no_bubble" linkType="instagram"/>
+              </div>
+          </div>
+        );
+    },
+
+    handleScroll: function () {
+        window.addEventListener('scroll', this.getStickyBar)
+    },
+
+    getStickyBar: function (e) {
+        var scrollY = e.path[0].scrollY || e.path[1].scrollY;
+        console.log(scrollY)
+        if (scrollY >= 120) {
+          this.setState({ stickyBarClass: 'navigation-sticky--shown'})
+        } else {
+          this.setState({ stickyBarClass: 'navigation-sticky--hidden'})
+        }
+
+        return scrollY;
+    },
+
+    getLinks: function () {
+      return (
+          <div className="navigation-sticky">
+              <div className="navigation-sticky-links"><a href="#">Novedades</a></div>
+              <div className="navigation-sticky-links"><a href="#">Deportes</a></div>
+              <div className="navigation-sticky-links"><a href="#">Life Style</a></div>
+              <div className="navigation-sticky-links"><a href="#">Turismo</a></div>
+              <div className="navigation-sticky-links"><a href="#">Ocio</a></div>
+              <div className="navigation-sticky-links"><a href="#">Tecno</a></div>
+              <div className="navigation-sticky-links"><a href="#">Sociales</a></div>
+              <div className="navigation-sticky-links"><a href="#">Concepto Tv</a></div>
+          </div>
+      );
+    }
+});
+
+module.exports = NavigationSticky;

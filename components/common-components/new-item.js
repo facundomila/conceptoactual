ad07ff1d3d2a7a6bd0542bec1dbcb49bd143c3ab
fmila <fmila@flexitifinancial.com>
2018-01-18 16:56:23 -0300

    fix issues

--- components/common-components/new-item.js
+++ components/common-components/new-item.js
@@ -7,14 +7,14 @@ var NewItem = React.createClass({
     render: function () {
         var news = this.props.children;
         var category = (news) ? news.category_id : null;
-
+        console.log(category)
         return (
-            <div className={this.getClass(news)+category}>
-              <a href={this.getLocation(news)}>
-                {this.renderTitle(news)}
-                {this.renderCategory(news)}
-                {this.renderImg(news)}
-              </a>
+            <div className={this.getClass(news) + category}>
+                <a href={this.getLocation(news)}>
+                    {this.renderTitle(news)}
+                    {this.renderCategory(news)}
+                    {this.renderImg(news)}
+                </a>
             </div>
         );
     },
@@ -29,21 +29,21 @@ var NewItem = React.createClass({
     },
 
     getLocation: function (news) {
-        var location = (news) ? "news.html?id="+news.id : 'news.html?id=0';
+        var location = (news) ? "news.html?id=" + news.id : 'news.html?id=0';
 
         return location;
     },
 
     renderTitle: function (news) {
-        var titleClass = "new-item-"+this.props.type+"--title";
+        var titleClass = "new-item-" + this.props.type + "--title";
         var title = (news) ? <div className={titleClass}>{news.title}</div> : null;
 
         return title;
     },
 
     renderCategory: function (news) {
-        var categoryClass = "new-item-"+this.props.type+"-category--";
-        var category = (news) ? <div className={categoryClass+news.category_id}>{news.category_name}</div> : null;
+        var categoryClass = "new-item-" + this.props.type + "-category--";
+        var category = (news) ? <div className={categoryClass + news.category_id}>{news.category_name}</div> : null;
 
         return category;
     },
@@ -52,7 +52,7 @@ var NewItem = React.createClass({
         var ruta;
         var rutaMini;
         var dynamicPath = "http://conceptoactual.com/cpanel/";
-        var imgClass = "new-item-"+this.props.type+"--img";
+        var imgClass = "new-item-" + this.props.type + "--img";
         if (news) {
             ruta = news.ruta || null;
             rutaMini = news.ruta_miniatura || null;
@@ -60,7 +60,7 @@ var NewItem = React.createClass({
             null
         }
         var size = (this.props.type === "xone") ? ruta : rutaMini;
-        var renderImg = <img width="100%" className={imgClass} src={"http://conceptoactual.com/cpanel/"+size}/>;
+        var renderImg = <img width="100%" className={imgClass} src={"http://conceptoactual.com/cpanel/" + size} />;
 
         return (news) ? renderImg : null;
     },
